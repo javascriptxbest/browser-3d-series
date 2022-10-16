255efbd1502f9f6aa363bc0f68aea67e785247a7
Louis Foster <123@louisfoster.com>
2022-10-16 20:50:45 +0200

    update pages deploy script

--- pages.sh
+++ pages.sh
@@ -1,1 +1,15 @@
-git checkout -b gh-pages
+git checkout gh-pages || git checkout -b gh-pages || exit 1
+mkdir output
+mkdir output/hello-world
+cd hello-world
+yarn
+yarn generate
+cp -r dist/. ../output/hello-world
+cd ..
+rm -rf hello-world
+mv output/* .
+rm -rf output
+rm pages.sh
+git add .
+git commit -m "update pages"
+git push -u origin gh-pages
